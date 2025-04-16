6d706e31442e8cf23f18c283d740bd172ba0bd29
dave <shadow2272@aol.com>
2025-04-16 21:39:28 +0100

    Game ticker and typwriter function

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ function counter() {
     document.getElementById("game-timer").innerHTML = ++gameTime;
     document.getElementById("start-game-button").disabled = true
 }
+
+function typeWriter(outputMessage, htmlID) {
+    let i = 0;
+    let txt = outputMessage;
+    let speed = 50;
+
+    if (i < txt.length) {document.getElementById(htmlID).innerHTML =+ txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
