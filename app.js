71b9cd7ac6f220d568df6e1394655b4639d2db05
Jimud1 <148221924+Jimud1@users.noreply.github.com>
2025-04-17 10:47:39 +0100

    image canvas

--- app.js
+++ app.js
@@ -10,12 +10,51 @@ const chinaId = 2;
 let countries = [
   { id: 1, name: 'UK', tarrif: 0 },
   { id: chinaId, name: 'China', tarrif: 0 },
-  { id: 3, name: 'EU', tarrif: 0 }
+  { id: 3, name: 'EU', tarrif: 0 },
+  { id: 4, name: 'Mexico', tarrif: 0 },
+  { id: 5, name: 'Latin America', tarrif: 0 },
+  { id: 6, name: 'Africa', tarrif: 0 },
+  { id: 7, name: 'Canada', tarrif: 0 },
+  { id: 8, name: 'Russia', tarrif: 0 },
+  { id: 9, name: 'Asia', tarrif: 0 },
+  { id: 10, name: 'Australia', tarrif: 0 }
 ];
 
 const container = document.getElementById('button-container');
 
-loadCountries = () => {
+const canvas = document.getElementById('viewport'),
+context = canvas.getContext('2d');
+
+const image = new Image();
+
+loadImage();
+
+function loadImage() {
+  image.src = 'world-map.jpeg';
+  image.onload = function(){
+    context.drawImage(image, 0, 0, image.width, image.height);
+    
+  }
+}
+
+canvas.addEventListener('click', (event) => {
+  const rect = canvas.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+  const highlightSize = 50; // Size of the highlighted section
+
+  // Redraw the image
+  context.drawImage(image, 0, 0, image.width, image.height);
+
+  // Highlight the clicked section
+  context.strokeStyle = 'red';
+  context.lineWidth = 5;
+  context.strokeRect(x - highlightSize / 2, y - highlightSize / 2, highlightSize, highlightSize);
+  console.log('clicked x ', x);
+  console.log('clicked y ', y);
+});
+
+loadCountryButtons = () => {
   countries.forEach(country => {
     const button = document.createElement('button');
     button.textContent = country.name + ' ' + country.tarrif; 
@@ -29,10 +68,10 @@ removeCountryButtons = () => {
   buttons.forEach(button => button.remove());
 }
 
-loadCountries();
+loadCountryButtons();
 
 addTarrif = (countryId, tarrif) => {
-  let country = countries.find(c => c.id == countryId);
+  const country = countries.find(c => c.id == countryId);
   country.tarrif = country.tarrif + tarrif;
   countries = countries.map(c => c.id === countryId ? country : c);
   console.log(countries);
