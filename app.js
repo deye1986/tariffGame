8c73366e5106eb158a5963a5f65c67157a7dd5a9
dave <shadow2272@aol.com>
2025-04-17 04:29:34 +0100

    game ticker function working. Typewriter effect removed.

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 let gameTime = 0;
 
+
 function counter() {
     document.getElementById("game-timer").innerHTML = ++gameTime;
-    document.getElementById("start-game-button").disabled = true
+    document.getElementById("start-game-button").disabled = true;
 }
 
-function typeWriter(outputMessage, htmlID) {
-    let i = 0;
-    let txt = outputMessage;
-    let speed = 50;
-
-    if (i < txt.length) {document.getElementById(htmlID).innerHTML =+ txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
+function displayMessageOnTicker(outputMessage) {
+    document.getElementById("game-ticker").innerHTML = outputMessage;
 }
+
