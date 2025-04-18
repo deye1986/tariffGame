5d4ffacabca6e655bae2e7dc761f3e63e08296bf
Jimud1 <148221924+Jimud1@users.noreply.github.com>
2025-04-18 18:25:27 +0100

    resized image so all map is available to click add tarrif by clicking the map

--- app.js
+++ app.js
@@ -21,25 +21,25 @@ let countries = [
     imgCords: { x: 580, y: 350, width: 120, height: 200 } },
   { id: 4, name: 'Mexico', tarrif: 0,
     imgCords: { x: 90, y: 550, width: 120, height: 100 }
-   },
+  },
   { id: 5, name: 'Latin America', tarrif: 0,
     imgCords: { x: 250, y: 700, width: 200, height: 200 }
-   },
+  },
   { id: 6, name: 'Africa', tarrif: 0,
     imgCords: { x: 580, y: 600, width: 300, height: 300 }
-   },
+  },
   { id: 7, name: 'Canada', tarrif: 0,
     imgCords: { x: 100, y: 280, width: 200, height: 250 }
-   },
+  },
   { id: 8, name: 'Russia', tarrif: 0,
     imgCords: { x: 800, y: 300, width: 300, height: 180 }
-   },
+  },
   { id: 9, name: 'Asia', tarrif: 0,
     imgCords: { x: 850, y: 580, width: 200, height: 150 }
-   },
+  },
   { id: 10, name: 'Australia', tarrif: 0,
     imgCords: { x: 970, y: 730, width: 100, height: 100 }
-   }
+  }
 ];
 
 const container = document.getElementById('button-container');
@@ -76,14 +76,15 @@ canvas.addEventListener('click', (event) => {
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
 
-  const cords = countries.filter(c => c.imgCords != null);
+  const cords = countries.filter(
+    c => c.imgCords != null
+    && x >= (c.imgCords.x - c.imgCords.width / 2)
+    && x <= (c.imgCords.x + c.imgCords.width / 2)
+    && y >= (c.imgCords.y - c.imgCords.height / 2)
+    && y <= (c.imgCords.y + c.imgCords.height / 2));
 
   cords.forEach(c => {
-    const area = c.imgCords;
-    console.log('area', area);
-    console.log('x ' + x);
-    console.log('y ' + y);
-
+    addTarrif(c.id, 10);
   });
 });
 
@@ -107,7 +108,6 @@ addTarrif = (countryId, tarrif) => {
   const country = countries.find(c => c.id == countryId);
   country.tarrif = country.tarrif + tarrif;
   countries = countries.map(c => c.id === countryId ? country : c);
-  console.log(countries);
   removeCountryButtons();
   loadCountryButtons();
   chinaResponse(country);
@@ -122,7 +122,6 @@ const chinaResponses = [
 chinaResponse = (country) => {
   const response = chinaResponses.find(cr => cr.tarrif == country.tarrif);
   if (country.id === chinaId && response) {
-    alert(response.response);
     displayMessageOnTicker(response.response);
   }
 }
