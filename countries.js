ac38a1fbcfe5c5b85f7a6297834e3d9f3ed31cc8
Jimud1 <148221924+Jimud1@users.noreply.github.com>
2025-04-24 13:44:16 +0100

    split out countries

---
+++ countries.js
@@ -0,0 +1,81 @@
+const defaultCountries = [
+  { 
+    "id": 1, 
+    "name": "UK", 
+    "tariff": 0, 
+    "imgCords": { "x": 475, "y": 375, "width": 50, "height": 50 },
+    "responses": [
+      { "tariff": 40, "response": "Blimey!" },
+      { "tariff": 50, "response": "My goodness, the special relationship has been shattered!" },
+      { "tariff": 80, "response": "Britain will no longer export to the USA and seeks a willing nation with lower tariffs to handle its imports" }
+    ],
+    "flagImage": "",
+    "cashReserves": 0,
+    "marketVolatility": 0.05 
+  },
+  { "id": 2, 
+    "name": "China", 
+    "tariff": 0, 
+    "imgCords": { "x": 800, "y": 450, "width": 300, "height": 100 },
+    "responses": [
+      { "tariff": 40,  "response": "Oh you better not you god damn America, we will not be bullied!" },
+      { "tariff": 60,  "response": "Oh no, not cool America, we will sell your bonds if you carry on" },
+      { "tariff": 100, "response": "We don't care America, you need us more than we need you!" },
+      { "tariff": 120, "response": "We are now applying a tarrif back on you for the same rate America!" }
+    ],
+    "flagImage": "flags/flagImageChina.png",
+    "cashReserves": 0,
+    "marketVolatility": 0.05
+  },    
+  { "id": 3, 
+    "name": "EU", 
+    "tariff": 0, 
+    "imgCords": { "x": 580, "y": 350, "width": 120, "height": 200 },
+    "responses": [
+      { "tariff": 30, "response": "Don't be stupid America! We will need you to calm down before the next meeting" },
+      { "tariff": 60, "response": "We will replace what we buy off you from China if you carry on America" }
+    ]
+  },
+  { "id": 4, 
+    "name": "Mexico", 
+    "tariff": 0,
+    "imgCords": { "x": 90, "y": 550, "width": 120, "height": 100 },
+    "responses": [
+      { "tariff": 20, "response": "Hey gringo, don't be like that. You know we have a special relationship" }
+    ]
+  },
+  { "id": 5, 
+    "name": "Latin America", 
+    "tariff": 0,
+    "imgCords": { "x": 250, "y": 700, "width": 200, "height": 200 },
+    "responses": [
+      { "tariff": 20, "response": "Well this is no surprise at all" },
+      { "tariff": 40, "response": "Oh we are so through with this, we arent giving you any Canals, stop trying to bully us" }
+    ]
+  },
+  { "id": 6, 
+    "name": "Africa", 
+    "tariff": 0,
+    "imgCords": { "x": 580, "y": 600, "width": 300, "height": 300 }
+  },
+  { "id: 8, ": 7, 
+    "name": "Canada", 
+    "tariff": 0,
+    "imgCords": { "x": 100, "y": 280, "width": 200, "height": 250 }
+  },
+  { "id": 8, 
+    "name": "Russia", 
+    "tariff": 0,
+    "imgCords": { "x": 800, "y": 300, "width": 300, "height": 180 }
+  },
+  { "id": 9, 
+    "name": "Asia", 
+    "tariff": 0,
+    "imgCords": { "x": 850, "y": 580, "width": 200, "height": 150 }
+  },
+  { "id": 10, 
+    "name": "Australia", 
+    "tariff": 0,
+    "imgCords": { "x": 970, "y": 730, "width": 100, "height": 100 }
+  }
+]
