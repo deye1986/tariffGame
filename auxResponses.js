6f0fc69b213467b0d8eab1a6bfc1d8c4693943ef
dave <shadow2272@aol.com>
2025-05-27 16:09:55 +0100

    Introduction file created containg scalable response json data for in game events not related to a particualar nation.

---
+++ auxResponses.js
@@ -0,0 +1,22 @@
+// this file contains the usa and intoduction respoinses which ideally
+// should appear in a dialog element, once immidiateley on the game boot, 
+// and THEN when ever required after - possibly to narate the game from the 
+// players/USA's perspective.
+// 
+// it doesnt require coords for the map but does use an flagImage file (usa)
+
+// i propose a function (in app.js) that measures events in game and displays: helper dialogs,
+// warning dialogues, and funny instances not related to a particualar nation.
+// the function should check if a threshold is reached and then display a response
+// from the json below, depending on the state of the game.
+
+const auxillaryResponses = 
+    { 
+      "id": 25, 
+      "name": "USA", 
+      "responses": [
+        { "signal": 1, "response": "Hi, welcome to Trump Tariff, click the countries buttons to get started." },
+        { "signal": 2, "response": "a response for somthing" },
+      ],
+      "flagImage": "flags/flagImageUSA.svg",
+    }
