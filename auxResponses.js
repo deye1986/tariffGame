0de5370bc1ada86f519d66726721c2a80089500e
dave <shadow2272@aol.com>
2025-05-28 11:20:52 +0100

    Bug TAR18 fixed & removed redundant 'mergers' folder.

--- auxResponses.js
+++ auxResponses.js
@@ -1,14 +1,15 @@
 // this file contains the usa and intoduction respoinses which ideally
 // should appear in a dialog element, once immidiateley on the game boot, 
-// and THEN when ever required after - possibly to narate the game from the 
+// and when ever required after - to narate the game from the 
 // players/USA's perspective.
 // 
 // it doesnt require coords for the map but does use an flagImage file (usa)
 
-// i propose a function (in app.js) that measures events in game and displays: helper dialogs,
-// warning dialogues, and funny instances not related to a particualar nation.
-// the function should check if a threshold is reached and then display a response
-// from the json below, depending on the state of the game.
+// i propose a function (in app.js) that leverages 'calculateAllTariffs()' func 
+// in game and displays helper dialogs, warning dialogues, and funny instances 
+// not related to a particualar nation. The function should check if a 
+// threshold is reached and then display a response from the json below, 
+// depending on the state of the game.
 
 const auxillaryResponses = 
     { 
